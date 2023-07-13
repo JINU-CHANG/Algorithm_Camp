af0fe74b010aae993407a5fb6f48be3f66925905
JINU-CHANG <jenny010925@naver.com>
2023-07-14 02:23:33 +0900

    H_5333 solve

---
+++ week1/H_5333.java
@@ -0,0 +1,21 @@
+package week1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class H_5333 {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long sum1 = 1L;
+        long sum2 = 1L;
+        for(int i=2; i<N; i++){
+            long temp = sum2;
+            sum2 = (sum1 + sum2);
+            sum1 = temp;
+        }
+
+        System.out.println(sum2);
+    }
+    
+}
