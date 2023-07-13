1521f784963b1b71c21aeccc64e4722e0dba3c58
JINU-CHANG <jenny010925@naver.com>
2023-07-14 02:38:31 +0900

    H_Fibo2 solve

---
+++ week1/H_Fibo2.java
@@ -0,0 +1,20 @@
+package week1;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class H_Fibo2 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        BigInteger sum1 = new BigInteger("1");
+        BigInteger sum2 = new BigInteger("1");
+        for(int i=2; i<N; i++){
+            BigInteger temp = sum2;
+            sum2 = (sum1.add(sum2));
+            sum1 = temp;
+        }
+        BigInteger x = new BigInteger("1000000007");
+        System.out.println(sum2.remainder(x));
+    }
+}
