d35730942e485c95ade9c8208aa42ee39e50d4d4
JINU-CHANG <jenny010925@naver.com>
2023-08-07 13:22:36 +0900

    4677 solve

---
+++ week5/H_4677.java
@@ -0,0 +1,23 @@
+package week5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class H_500 {
+        public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        HashSet<Integer> hset = new HashSet<>();
+
+        int N = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            hset.add(Integer.parseInt(st.nextToken()));
+        }
+        System.out.println(hset.size());
+    }
+}
